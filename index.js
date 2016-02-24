5e4f5bd9e6980e790b795722309982e3ba140bf0
Guillermo Rodas <grodas@hugeinc.com>
2016-02-24 17:03:02 -0500

    Infinite recursive function solved

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
+const tokens = require('./tokens');
 const Twit = require('twit');
 
-const T = new Twit({
-  consumer_key: 'n0wm5PsJHfNJSXl1KQFMttvTF',
-  consumer_secret: '9HkO9UUH0oE5h1RuJkAYvt6DZPXn9Rb9QFRQUHDDvV0dYBHX72',
-  access_token: '18968309-p2Khy4SoBUftQReJQ0X3An57ekdFau03TL4D0XSdf',
-  access_token_secret: 'SUK2KblWR9oAijEMX7aaRx2GZYjG6XQzsdLNnYWFYOv3u'
+const twitter = new Twit({
+  consumer_key:        tokens.CONSUMER_KEY,
+  consumer_secret:     tokens.CONSUMER_SECRET,
+  access_token:        tokens.ACCESS_TOKEN,
+  access_token_secret: tokens.ACCESS_TOKEN_SECRET,
 });
 
 /**
@@ -14,22 +15,25 @@ const T = new Twit({
  * @param  {Function} callback [description]
  * @return {[type]}            [description]
  */
-function getFriendsIds(friendsList, callback) {
+function getFriendsIds(friendsList, cursor, callback) {
   let currentFriendsList = friendsList || [];
+  let currentCursor = cursor || -1;
 
-  T.get('friends/list', {count: 200}, (err, data, response) => {
+  twitter.get('friends/list', {count: 200, cursor: currentCursor}, (err, data, response) => {
     if (!err) {
       currentFriendsList.push(data.users.map(user => {
         return { userId: user.id, userName: user.screen_name };
       }));
 
+      console.log(currentCursor);
+
       if (data.next_cursor) {
-        getFriendsIds(currentFriendsList, callback);
+        getFriendsIds(currentFriendsList, data.next_cursor, callback);
       } else {
         callback(currentFriendsList);
       }
     } else {
-      console.error(err);
+      console.error('GET_FRIENDS_IDS_ERR:', err);
     }
   });
 };
@@ -39,13 +43,13 @@ function getFriendsIds(friendsList, callback) {
  * @return {[type]} [description]
  */
 function getListsIds(callback) {
-  T.get('lists/list', {count: 100, reverse: true }, (err, data, response) => {
+  twitter.get('lists/list', {count: 100, reverse: true }, (err, data, response) => {
     if (!err) {
       callback(data.map(list => {
         return { listId: list.id, listName: list.name };
       }));
     } else {
-      console.error(err);
+      console.error('GET_LISTS_IDS_ERR:', err);
     }
   });
 }
@@ -62,7 +66,7 @@ function getListsMembers(lists, index, usersList, callback) {
   let currentIndex = index || 0;
   let currentUsersList = usersList || [];
 
-  T.get('lists/members', {list_id: lists[currentIndex].listId }, (err, data, response) => {
+  twitter.get('lists/members', {list_id: lists[currentIndex].listId }, (err, data, response) => {
     if (!err) {
       currentUsersList.push(data.users.map(user => {
         return { userId: user.id, userName: user.screen_name };
@@ -76,26 +80,33 @@ function getListsMembers(lists, index, usersList, callback) {
         callback(currentUsersList);
       }
     } else {
-      console.error(err);
+      console.error('GET_LISTS_MEMBERS_ERR:', err);
     }
   });
 }
 
-getFriendsIds(null, friends => {
 
-  getListsIds(lists => {
+function init() {
+  getFriendsIds(null, friends => {
+    console.info('FRIENDS_LENGTH:', friends.length);
 
-    getListsMembers(lists, null, null, users => {
-      console.log('USERS', users.length);
-      console.log('FRIENDS', friends.length);
+    getListsIds(lists => {
+      console.info('LIST_LENGTH:', lists.length);
 
-      const unlistedFriends = friends.filter(friend => {
-        return users.filter(user => {
-          return user.userId === friend.userId;
-        }).length === 0;
+      getListsMembers(lists, null, null, users => {
+        console.info('USERS_LENGTH:', users.length);
+
+        const unlistedFriends = friends.filter(friend => {
+          return users.filter(user => {
+            return user.userId === friend.userId;
+          }).length === 0;
+        });
+
+        console.log(unlistedFriends);
       });
-
-      console.log(unlistedFriends);
     });
   });
-});
+}
+
+//init();
+getFriendsIds(null, null, friends => { console.info('FRIENDS_LENGTH:', friends.length); });
