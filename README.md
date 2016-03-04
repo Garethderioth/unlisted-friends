803089984b06bf2c761c08aa315410e0504afcc3
Guillermo Rodas <grodas@hugeinc.com>
2016-03-04 11:35:58 -0500

    Improving file structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# Keys
+Visit https://dev.twitter.com/apps and register a new application. Go to the application 'Keys and Access Tokens' page and click on "Create my access token" to generate your access tokens.
--- index.js
+++ index.js
@@ -1,104 +1,31 @@
 'use strict';
 
-const tokens = require('./tokens');
-const Twit = require('twit');
-
-const twitter = new Twit({
-  consumer_key: tokens.CONSUMER_KEY,
-  consumer_secret: tokens.CONSUMER_SECRET,
-  app_only_auth: true,
-});
-
-/**
- * [getFriendsIds description]
- * @param  {Function} callback [description]
- * @return {[type]}            [description]
- */
-function getFriendsIds(username, friendsList, cursor, callback) {
-  let currentFriendsList = friendsList || [];
-  let currentCursor = cursor || -1;
-
-  twitter.get('friends/list', {count: 200, cursor: currentCursor, screen_name: username}, (err, data, response) => {
-    if (!err) {
-      currentFriendsList = currentFriendsList.concat(data.users.map(user => {
-        return { userId: user.id, userName: user.screen_name };
-      }));
-
-      if (data.next_cursor) {
-        getFriendsIds(username, currentFriendsList, data.next_cursor, callback);
-      } else {
-        callback(currentFriendsList);
-      }
-    } else {
-      console.error('GET_FRIENDS_IDS_ERR:', err);
-    }
-  });
-};
-
-/**
- * [getListsIds description]
- * @return {[type]} [description]
- */
-function getListsIds(username, callback) {
-  twitter.get('lists/list', {count: 100, reverse: true, screen_name: username }, (err, data, response) => {
-    if (!err) {
-      callback(data.map(list => {
-        return { listId: list.id, listName: list.name };
-      }));
-    } else {
-      console.error('GET_LISTS_IDS_ERR:', err);
-    }
-  });
-}
-
-/**
- * [getListsMembers description]
- * @param  {[type]}   lists     [description]
- * @param  {[type]}   index     [description]
- * @param  {[type]}   usersList [description]
- * @param  {Function} callback  [description]
- * @return {[type]}             [description]
- */
-function getListsMembers(lists, index, usersList, callback) {
-  let currentIndex = index || 0;
-  let currentUsersList = usersList || [];
-
-  twitter.get('lists/members', {list_id: lists[currentIndex].listId, count: 5000 }, (err, data, response) => {
-    if (!err) {
-      currentUsersList = currentUsersList.concat(data.users.map(user => {
-        return { userId: user.id, userName: user.screen_name };
-      }));
-
-      currentIndex += 1;
-
-      if (lists[currentIndex]) {
-        getListsMembers(lists, currentIndex, currentUsersList, callback);
-      } else {
-
-        callback(currentUsersList);
-      }
-    } else {
-      console.error('GET_LISTS_MEMBERS_ERR:', err);
-    }
-  });
-}
+const friends = require('./lib/friends');
+const lists = require('./lib/lists');
+const members = require('./lib/members');
 
 /**
  * [init description]
- * @return {[type]} [description]
+ * @param  {[type]} username [description]
+ * @return {[type]}          [description]
  */
 function init(username) {
-  getFriendsIds(username, null, null, friends => {
-    //console.info('FRIENDS_LENGTH:', friends.length);
+  friends(username, null, null, friendList => {
+    console.info('FRIENDS_LENGTH:', friendList.length);
 
-    getListsIds(username, lists => {
-      //console.info('LIST_LENGTH:', lists.length);
+    lists(username, userLists => {
+      console.info('LIST_LENGTH:', userLists.length);
 
-      getListsMembers(lists, null, null, users => {
-        //console.info('USERS_LENGTH:', users.length);
+      if(!userLists.length) {
+        console.log('You dont have lists, or your lists are privated');
+        return;
+      }
 
-        const unlistedFriends = friends.filter(friend => {
-          return users.filter(user => {
+      members(userLists, null, null, member => {
+        console.info('USERS_LENGTH:', member.length);
+
+        const unlistedFriends = friendList.filter(friend => {
+          return member.filter(user => {
             return user.userId === friend.userId;
           }).length === 0;
         });
@@ -115,5 +42,5 @@ function init(username) {
   });
 }
 
+// TODO: Move this to UI
 init('Garethderioth');
-//getFriendsIds(null, null, friends => { console.info('FRIENDS_LENGTH:', friends.length); });
