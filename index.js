fa83d52ddd2c223cc14f2fb1ef18619b60862289
Guillermo Rodas <grodas@hugeinc.com>
2016-03-15 14:35:28 -0500

    Improve readability

--- index.js
+++ index.js
@@ -7,38 +7,70 @@ const members = require('./lib/members');
 
 /**
  * [printFriends description]
+ * @param  {[type]} username    [description]
  * @param  {[type]} friendsList [description]
  * @return {[type]}             [description]
  */
-function printFriends(friendsList) {
-  return friendsList && friendsList.length ? '\n' + friendsList
-    .map(friend => (`https://twitter.com/${friend.name}`))
-    .join('\n') : '\nCongratulations! All your friends are in a list!';
+function printFriends(username, friendsList) {
+  let result = [`Congratulations @${username}! All your friends are in a list!`];
+
+  if (friendsList && friendsList.length) {
+    result = friendsList.map(friend => (`https://twitter.com/${friend.name}`));
+  }
+
+  console.log(result.join('\n'));
 }
 
 /**
- * [init description]
- * @param  {[type]} username [description]
- * @return {[type]}          [description]
+ * [printSummary description]
+ * @param  {[type]} friendsList     [description]
+ * @param  {[type]} userLists       [description]
+ * @param  {[type]} membersList     [description]
+ * @param  {[type]} unlistedFriends [description]
+ * @return {[type]}                 [description]
  */
-function init(username) {
-  Promise.all([friends(username), lists(username)]).then(response => {
-    const friendsList = response[0];
-    const userLists = response[1];
+function printSummary(friendsList, userLists, membersList, unlistedFriends) {
+  const summary = [
+    `Friends: ${friendsList.length}`,
+    `Lists: ${userLists.length}`,
+    `Members: ${membersList.length}`,
+    `Unlisted friends: ${unlistedFriends.length}`
+  ];
 
-    members(userLists).then(membersList => {
-      console.info(`Friends: ${friendsList.length}`);
-      console.info(`Lists: ${userLists.length}`);
-      console.info(`Members: ${membersList.length}`);
+  console.log(summary.join('\n'));
+}
 
-      console.log(printFriends(filterFriends(friendsList, membersList)));
-    }, reason => {
-      console.error(reason);
-    });
+/**
+ * [getMembers description]
+ * @param  {[type]} friendsList [description]
+ * @param  {[type]} userLists   [description]
+ * @return {[type]}             [description]
+ */
+function getMembers(friendsList, userLists) {
+  members(userLists).then(membersList => {
+    const unlistedFriends = filterFriends(friendsList, membersList);
+
+    printSummary(friendsList, userLists, membersList, unlistedFriends);
+    printFriends(username, unlistedFriends);
   }, reason => {
     console.error(reason);
   });
 }
 
-// TODO: Move to as a service
-init('Garethderioth');
+/**
+ * [getUnlistedFriends description]
+ * @param  {[type]} username [description]
+ * @return {[type]}          [description]
+ */
+function getUnlistedFriends(username) {
+  Promise.all([
+    friends(username),
+    lists(username)
+  ]).then(response => {
+    getMembers(response[0], response[1]);
+  }, reason => {
+    console.error(reason);
+  });
+}
+
+getUnlistedFriends(process.argv[2]);
