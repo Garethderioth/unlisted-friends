182bec4c6a34b83cc1d4006ee651e20b54fcb29a
Guillermo Rodas <garethderioth@gmail.com>
2016-07-28 01:46:40 -0500

    fix(Travis build): Fixing the connectToTwitter parameters for travis.

--- tests/friends.test.js
+++ tests/friends.test.js
@@ -6,7 +6,11 @@ import keys from './keys';
 test(`Given an username
       When I ask for his/her friend list
       Then I should have his/her friend list.`, t => {
-  const Twitter = connectToTwitter(keys.CONSUMER_KEY, keys.CONSUMER_SECRET);
+  const Twitter = connectToTwitter(
+    keys.CONSUMER_KEY || undefined,
+    keys.CONSUMER_SECRET || undefined
+  );
+
   const username = 'Garethderioth';
 
   return friends(Twitter, username).then(friendList => {
@@ -17,7 +21,10 @@ test(`Given an username
 test(`Given an username that doesn\'t follow anyone
       When I ask for his/her friend list
       Then I should throw an error.`, t => {
-  const Twitter = connectToTwitter(keys.CONSUMER_KEY, keys.CONSUMER_SECRET);
+  const Twitter = connectToTwitter(
+    keys.CONSUMER_KEY || undefined,
+    keys.CONSUMER_SECRET || undefined
+  );
   const username = 'Pictoline';
 
   t.throws(friends(Twitter, username));
